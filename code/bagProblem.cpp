6a832db7aeb7847f5bf89cc8cf3eab9e3d74c69f
jakemmmr <1586590320@qq.com>
2020-10-23 13:48:19 +0800

    useable

--- code/bagProblem.cpp
+++ code/bagProblem.cpp
@@ -1,84 +1,52 @@
-#include <iostream>
-#include<cmath>
+/* This is a way using binary bits to solve the knapsack problem
+**For a bit, 1 means inclueded and 0 means exclueded.
+**
+** author: youpen_y
+*/
 
+#include<iostream>
+#include<cmath>
 using namespace std;
 
-class thing{
-private:
-    int volume;
-    int value;
-public:
-    thing()
-    {
-        volume=0;
-        value=0;
-    }
-    thing(int a,int b)
-    {
-        volume = a;
-        value = b;
-    }
-    int getvolume()
-    {
-        return volume;
-    }
-    int getvalue()
-    {
-        return value;
-    }
-    void changevolume(int a)
-    {
-        volume = a;
-    }
-    void changevalue(int b)
-    {
-        value = b;
-    }
-    ~thing(){}
-};
+//Function to show the maxvalue,
+//it takes the weight[], value[], and totalVolume: W, and objects totalNumber: N as parameters
+void knapsack(int weight[],int value[],int W,int N)
+{
 
+    int maxvalue=0;           
+    for(int i=0;i<pow(2,N);i++)  //there, i will go through the 2^n cases
+    {
+        int temp = i;           //can't change i in the loop, else you will change the iteration times, so pass its value to another local varible
+        int tempweight=0;
+        int tempvalue=0;
+        for(int j=0;j<N;j++)
+        {
+            if(temp%2==1)   //which means the jth bit is 1 
+            {
+                tempweight+=weight[j];
+                tempvalue+=value[j];
+            }
+            temp=temp/2;   //this sentence equal to  i>>1
+
+            if(tempweight<=W)
+            {
+                if(tempvalue>maxvalue)
+                    maxvalue=tempvalue;
+            }
+            if(temp==0)     //conditions for breaking out the inner loop
+                break;
+        }
+    }
+    cout<<"The Maxvalue is "<<maxvalue<<endl;
+}
+
+// Driver code
 int main()
 {
-    int volume;
-    int n;
-    cout << "Please input the volume of the bag: "<<endl;
-    cin>>volume;
-    cout << "Please input the num of things"<<endl;
-    cin>>n;
-    thing *a = new thing[n];
-    for(int i=0;i<n;i++)
-    {
-        int tempa,tempb;
-        cout<<"The "<<i+1<<" thing's volume is : "<<endl;
-        cin>>tempa;
-        a[i].changevolume(tempa);
-        cout<<"The "<<i+1<<" thing's value is : "<<endl;
-        cin>>tempb;
-        a[i].changevalue(tempb);
-    }
-    int tempvolume=0;
-    int tempvalue=0;
-    int maxvalue=0;
-    for(int i=0;i<pow(2,n);i++)
-    {
-
-        for(int j=0;j<n;j++)
-        {
-            if(i%2==1)
-            {
-                tempvolume+=a[j].getvolume();
-                tempvalue+=a[j].getvalue();
-            }
-            i=i/2;
-        }
-        if(tempvolume<=volume)
-        {
-            cout<<"hello1"<<endl;
-            if(tempvalue>maxvalue)
-                maxvalue=tempvalue;
-        }
-    }
-    delete a;
-    cout<<"The MAXvalue is : "<<maxvalue<<endl;
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    knapsack(wt,val,W,n);
     return 0;
 }
